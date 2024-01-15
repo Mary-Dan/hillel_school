9f859c3b99ad0d9ed42b458686c7992fdd80e68c
Mary-Dan <152028992+Mary-Dan@users.noreply.github.com>
2024-01-15 21:45:13 +0300

    Add files via upload

--- homework_12/ex_3.py
+++ homework_12/ex_3.py
@@ -1,16 +1,16 @@
 def update_hero(**kwargs):
     with open("hero.ini", "r") as f:
         lines = f.readlines()
-    hero_data = {}
+    hero = {}
     for line in lines:
         key, value = line.strip().split("=")
-        hero_data[key] = value
+        hero[key] = value
 
     for key, value in kwargs.items():
-        if key in hero_data:
-            hero_data[key] = str(value)
+        if key in hero:
+            hero[key] = str(value)
 
     with open("hero.ini", "w") as f:
-        for key, value in hero_data.items():
+        for key, value in hero.items():
             f.write(f"{key} = {value}\n")
 update_hero(hero="Halk", power=450, Y=2.3)
