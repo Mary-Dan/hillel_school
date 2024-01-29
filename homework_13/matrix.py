27fa7c346c3cdbdb775379a7ce4e6e27d257df08
Mary-Dan <152028992+Mary-Dan@users.noreply.github.com>
2024-01-29 16:12:52 +0300

    Add files via upload

--- homework_13/matrix.py
+++ homework_13/matrix.py
@@ -1,33 +1,38 @@
 import random
+
 def create_matrix(M):
     matrix = [[random.randint(1, 50) for _ in range(M)] for _ in range(M)]
     return matrix
+
 def print_matrix(matrix):
     M = len(matrix)
+    column_sums = [0] * M
     for i in range(M):
         for j in range(M):
             print(matrix[i][j], end="\t")
+            column_sums[j] += matrix[i][j]
         print()
 
-    sums = [sum(column) for column in zip(*matrix)]
-    for s in sums:
+    sorted_indices = sorted(range(M), key=lambda x: column_sums[x])
+
+    for s in sorted(column_sums):
         print(f"{s}\t", end="")
     print()
+
 def sort_matrix(matrix):
     M = len(matrix)
-    sums = [0] * M
+    column_sums = [0] * M
     for j in range(M):
         for i in range(M):
-            sums[j] += matrix[i][j]
-    sorted_indices = sorted(range(M), key=lambda x: sums[x])
+            column_sums[j] += matrix[i][j]
 
-    odd_columns = [sorted_indices[i] for i in range(M) if i % 2 != 0]
-    for i in range(len(odd_columns)):
-        matrix.sort(key=lambda x: x[odd_columns[i]])
+    sorted_indices = sorted(range(M), key=lambda x: column_sums[x])
 
-    even_columns = [sorted_indices[i] for i in range(M) if i % 2 == 0]
-    for i in range(len(even_columns)):
-        matrix.sort(key=lambda x: x[even_columns[i]], reverse=True)
+    for i in range(M):
+        if i % 2 == 0:
+            matrix.sort(key=lambda x: x[sorted_indices[i]])
+        else:
+            matrix.sort(key=lambda x: x[sorted_indices[i]], reverse=True)
 
 if __name__ == "__main__":
     M = int(input("Введите размер таблицы: "))
